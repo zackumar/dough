a51e8f502350c44adfd8782c6552714e2890355d
Zack Umar <zackumar2@gmail.com>
2022-07-31 16:21:06 -0500

    Remove e2e test for now

--- app/components/__tests__/Header.test.tsx
+++ app/components/__tests__/Header.test.tsx
@@ -1,9 +1,19 @@
 import { render, screen } from "@testing-library/react";
-import { userInfo } from "os";
+import { MemoryRouter } from "react-router-dom";
 import { Header } from "../Header";
 
+vi.mock("~/utils", () => {
+  return {
+    useOptionalUser: () => undefined,
+  };
+});
+
 test("Header renders correctly", () => {
-  render(<Header />);
+  render(
+    <MemoryRouter>
+      <Header />
+    </MemoryRouter>
+  );
 
   expect(screen.getByText("dough.")).toBeInTheDocument();
 });
